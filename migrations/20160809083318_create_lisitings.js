7a5dbc6c3aa81f206ad7b4291fbc43254bf7bf98
Dax <limeside@gmail.com>
2016-08-09 08:36:13 -0500

    Add migrations for listings table. :rabbit:

---
+++ migrations/20160809083318_create_lisitings.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('listings', table => {
+    table.increments().primary().index(),
+    table.integer('user_id')
+      .references('id')
+      .inTable('users')
+      .onDelete('cascade'),
+    table.string('item_name'),
+    table.text('item_desc'),
+    table.float('price'),
+    table.timestamps()
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('listings');
+};
